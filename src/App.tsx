8f49043faac3b69a07ccd6cf2fccd6ca425bae76
Petro Maliar <petrmishataya@gmail.com>
2022-05-23 13:15:17 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function AppComponent() {
+  // todo: rename variable name
+  const hi = 'Hi';
+
+  const getURL = () => {
+    const url = document.location;
+    console.log('url: ', url.origin)
+    const a = 1;
+    const b = 33;
+    const result = a + b;
+    console.log('result: ', result);
+  };
+  getURL();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. {hi}
         </p>
         <a
           className="App-link"
@@ -23,4 +35,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppComponent;
